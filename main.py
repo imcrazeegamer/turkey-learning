e7dfbe44757e5546da876e90decab5527149f45c
Ariel Horwitz <_@_._>
2022-04-13 21:06:02 +0300

    tiny refactor

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from data import get_audio, get_labels
 
 
-if __name__ == '__main__':
+def main():
     training_data = get_audio()
     training_labels = get_labels(extra_dim=True)
     print('=== Data ===')
     print(f'training_data: {training_data.shape}')
     print(f'training_labels: {training_labels.shape}')
+
+
+if __name__ == '__main__':
+    main()
