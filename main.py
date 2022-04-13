3cd956675a578d0889a435fbfb56b21901003ab3
Moral <sn@k.e>
2022-04-13 15:27:45 +0300

    data.

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import pandas as pd
 
+output = pd.read_csv("data/sample_submission.csv")
 
-if __name__ == "__main__":
-    print("Moral Is the Admin")
+test_data = pd.read_json("data/test.json")
+train_data = pd.read_json("data/train.json")
+pd.set_option('display.max_columns', 20 )
+pd.set_option('display.max_rows' , 40)
+
+relevant_train_data = train_data.drop(['end_time_seconds_youtube_clip', 'start_time_seconds_youtube_clip'], axis=1)
+print(relevant_train_data.head())
